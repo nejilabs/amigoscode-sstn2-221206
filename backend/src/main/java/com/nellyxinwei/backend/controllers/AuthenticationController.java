cedd75a51229fc4a22c2cb6189122a5d888695b5
根島憲紀-魏新栩 <nelsonalbajr@yahoo.com>
2022-12-17 03:30:14 +0900

    10738 / 11632

---
+++ backend/src/main/java/com/nellyxinwei/backend/controllers/AuthenticationController.java
@@ -0,0 +1,38 @@
+package com.nellyxinwei.backend.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nellyxinwei.backend.config.JwtUtils;
+import com.nellyxinwei.backend.dto.AuthenticationRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+  private final AuthenticationManager authenticationManager;
+  private final UserDetailsService userDetailsService;
+  private final JwtUtils jwtUtils;
+
+  @PostMapping("/authenticate")
+  public ResponseEntity<String> authenticate(
+      @RequestBody AuthenticationRequest request) {
+    authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+    final UserDetails user = userDetailsService.loadUserByUsername(request.getEmail());
+    if (user != null) {
+      return ResponseEntity.ok(jwtUtils.generateToken(user));
+    }
+    return ResponseEntity.status(400).body("Some error has occured");
+  }
+}
