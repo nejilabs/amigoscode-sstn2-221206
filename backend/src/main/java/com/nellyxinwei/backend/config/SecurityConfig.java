ab28fb2f7579d4f50b45a6fc243ea72278481a10
根島憲紀-魏新栩 <nelsonalbajr@yahoo.com>
2022-12-17 02:05:19 +0900

    Update SecurityConfig.java

--- backend/src/main/java/com/nellyxinwei/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/nellyxinwei/backend/config/SecurityConfig.java
@@ -4,9 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+  private final JwtAthFilter jwtAthFilter;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -15,7 +21,7 @@ public class SecurityConfig {
         .anyRequest()
         .authenticated()
         .and()
-        .httpBasic();
+        .addFilterBefore(jwtAthFilter, UsernamePasswordAuthenticationFilter.class);
     return http.build();
   }
 
